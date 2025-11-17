6ad75f5a271b7afd5574784526bc80a4c0ffd448
bona.stella.91 <bona.stella.91@gmail.com>
2025-11-17 23:57:56 +0900

    🎉 init: 프로젝트 생성

---
+++ 01-spring-api-board/spring-api-board/src/main/java/com/github/stella/springapiboard/SpringApiBoardApplication.java
@@ -0,0 +1,13 @@
+package com.github.stella.springapiboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiBoardApplication.class, args);
+    }
+
+}
