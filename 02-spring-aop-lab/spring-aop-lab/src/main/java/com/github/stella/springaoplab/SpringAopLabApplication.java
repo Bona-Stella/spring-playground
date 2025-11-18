641028c888533515cb9068d0e9b9ace40fc6c894
bona.stella.91 <bona.stella.91@gmail.com>
2025-11-18 18:31:17 +0900

    🎉 init: 프로젝트 생성

---
+++ 02-spring-aop-lab/spring-aop-lab/src/main/java/com/github/stella/springaoplab/SpringAopLabApplication.java
@@ -0,0 +1,13 @@
+package com.github.stella.springaoplab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopLabApplication.class, args);
+    }
+
+}
---
+++ 02-spring-aop-lab/spring-aop-lab/src/test/java/com/github/stella/springaoplab/SpringAopLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stella.springaoplab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
