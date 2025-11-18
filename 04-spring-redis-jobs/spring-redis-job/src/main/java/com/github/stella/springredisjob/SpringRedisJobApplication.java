10d14a9b02eb3d2ef1511a993339b5b6248cc712
bona.stella.91 <bona.stella.91@gmail.com>
2025-11-18 21:26:45 +0900

    🎉 init: 프로젝트 생성

---
+++ 04-spring-redis-jobs/spring-redis-job/src/main/java/com/github/stella/springredisjob/SpringRedisJobApplication.java
@@ -0,0 +1,13 @@
+package com.github.stella.springredisjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisJobApplication.class, args);
+    }
+
+}
---
+++ 04-spring-redis-jobs/spring-redis-job/src/test/java/com/github/stella/springredisjob/SpringRedisJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stella.springredisjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
