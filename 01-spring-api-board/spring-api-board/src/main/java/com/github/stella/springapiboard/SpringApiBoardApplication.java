3657381b5015a1d087dd8ff96828e95e6da19857
bona.stella.91 <bona.stella.91@gmail.com>
2025-11-19 22:56:23 +0900

    refactor: config 파일 분리

--- 01-spring-api-board/spring-api-board/src/main/java/com/github/stella/springapiboard/SpringApiBoardApplication.java
+++ 01-spring-api-board/spring-api-board/src/main/java/com/github/stella/springapiboard/SpringApiBoardApplication.java
@@ -2,18 +2,10 @@ package com.github.stella.springapiboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
-@EnableCaching
-@EnableScheduling
 @SpringBootApplication
 public class SpringApiBoardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringApiBoardApplication.class, args);
     }
-
 }
