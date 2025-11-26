a98db28684282799d0889b26c89147267b6a01a8
bona.stella.91 <bona.stella.91@gmail.com>
2025-11-26 13:11:12 +0900

    init: 프로젝트 초기화

--- 03-spring-security-jwt/spring-security-jwt/src/main/java/com/github/stella/springsecurityjwt/auth/dto/SignupRequest.java
+++
@@ -1,8 +0,0 @@
-package com.github.stella.springsecurityjwt.auth.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record SignupRequest(
-        @NotBlank String username,
-        @NotBlank String password
-) {}
