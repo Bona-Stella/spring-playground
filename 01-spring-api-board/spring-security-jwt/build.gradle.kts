d706f9a31aa80c7227a5ff5d4ecaf6bb1a164388
bona.stella.91 <bona.stella.91@gmail.com>
2025-11-26 13:11:12 +0900

    init: 프로젝트 초기화

--- 01-spring-api-board/spring-security-jwt/build.gradle.kts
+++ 01-spring-api-board/spring-security-jwt/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.7"
+    id("org.springframework.boot") version "3.5.8"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -19,17 +19,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    // JWT
-    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
-    // Lombok(optional). 프로젝트 전역에서 사용하지 않고, 필요 시 수동 코딩으로 대체 가능
-    compileOnly("org.projectlombok:lombok:1.18.34")
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
--- 03-spring-security-jwt/spring-security-jwt/src/main/java/com/github/stella/springsecurityjwt/auth/dto/TokenResponse.java
+++
@@ -1,6 +0,0 @@
-package com.github.stella.springsecurityjwt.auth.dto;
-
-public record TokenResponse(
-        String accessToken,
-        String refreshToken
-) {}
