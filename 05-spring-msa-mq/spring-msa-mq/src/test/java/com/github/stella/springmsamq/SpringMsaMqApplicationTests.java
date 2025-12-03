61d4c013a68fdd7dd82f4285f78af6b77b77f408
bona.stella.91 <bona.stella.91@gmail.com>
2025-12-03 21:07:05 +0900

    🎉 init: 프로젝트 생성

---
+++ 05-spring-msa-mq/spring-msa-mq/src/test/java/com/github/stella/springmsamq/SpringMsaMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stella.springmsamq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsaMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
