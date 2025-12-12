0b7fb71aa5443166ce3c95f4355ebc68edd230b7
bona.stella.91 <bona.stella.91@gmail.com>
2025-12-12 23:49:11 +0900

    docs: 코드 설명 및 주석 적용

--- 05-spring-msa-mq/spring-msa-mq/auth/src/main/java/com/github/stella/springmsamq/auth/AuthApplication.java
+++ 05-spring-msa-mq/spring-msa-mq/auth/src/main/java/com/github/stella/springmsamq/auth/AuthApplication.java
@@ -4,14 +4,16 @@ import com.github.stella.springmsamq.auth.config.JwtProperties;
 import com.github.stella.springmsamq.common.exception.CommonExceptionConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)
+//@EnableConfigurationProperties(JwtProperties.class)
 @Import(CommonExceptionConfig.class)
+@ConfigurationPropertiesScan
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
