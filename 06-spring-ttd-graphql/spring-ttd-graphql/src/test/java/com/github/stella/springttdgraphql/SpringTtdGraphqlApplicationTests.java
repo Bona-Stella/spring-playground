7d87fd939d2573825a472a42aef978d54e43e968
bona.stella.91 <bona.stella.91@gmail.com>
2025-12-27 23:06:37 +0900

    🎉 init: 프로젝트 생성

---
+++ 06-spring-ttd-graphql/spring-ttd-graphql/src/test/java/com/github/stella/springttdgraphql/SpringTtdGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stella.springttdgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTtdGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
