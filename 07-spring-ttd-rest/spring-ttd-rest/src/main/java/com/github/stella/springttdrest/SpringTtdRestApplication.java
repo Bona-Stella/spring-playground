bd1ad6063c11e204d7fcbf33aee163bb43ed2c6e
bona.stella.91 <bona.stella.91@gmail.com>
2025-12-29 23:49:26 +0900

    🎉 init: 프로젝트 생성

---
+++ 07-spring-ttd-rest/spring-ttd-rest/src/main/java/com/github/stella/springttdrest/SpringTtdRestApplication.java
@@ -0,0 +1,13 @@
+package com.github.stella.springttdrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTtdRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTtdRestApplication.class, args);
+    }
+
+}
---
+++ 07-spring-ttd-rest/spring-ttd-rest/src/test/java/com/github/stella/springttdrest/SpringTtdRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stella.springttdrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTtdRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
